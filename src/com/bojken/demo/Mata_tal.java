77afee1b4ed8d7d05acd985bbc55a98aca8378e8
Bojken07 <bojken.zaimi@stud.sti.se>
2023-09-21 18:04:27 +0200

    uppgift_2

---
+++ src/com/bojken/demo/Mata_tal.java
@@ -0,0 +1,10 @@
+package com.bojken.demo;
+
+public class Mata_tal {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 7; i++) {
+            System.out.println("Say one number");
+        }
+    }
+}
